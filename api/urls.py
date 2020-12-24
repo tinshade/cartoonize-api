55809c2f1acc23671e240a8bebe8225042d7d602
tinshade <tinshade98@gmail.com>
2020-12-24 21:23:11 +0530

    Updating buildpack files

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from .views import Cartoonize
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns=[
-    path('cartoonize/', Cartoonize.as_view(), name="cartoonize+"),
+    path('cartoonize/', Cartoonize.as_view(), name="cartoonize"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
